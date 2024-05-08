28b7740483729eca90a35bad59d42cdaac35e322
Angel <angel.bautista@pucp.edu.pe>
2024-05-08 01:33:46 +0000

    AAA v1

--- CLI.py
+++ CLI.py
@@ -1,6 +1,70 @@
 import click
 from rich.console import Console
 from rich.table import Table
+import mariadb
+import hashlib
+import os
+import json
+import uuid
+
+
+IMAGE_DIR = '/imagenes/'
+IMAGE_DATA_FILE = os.path.join(IMAGE_DIR, 'image_data.json')
+
+def load_image_data():
+    if os.path.exists(IMAGE_DATA_FILE):
+        with open(IMAGE_DATA_FILE, 'r') as f:
+            return json.load(f)
+    else:
+        return {}
+
+def save_image_data(data):
+    with open(IMAGE_DATA_FILE, 'w') as f:
+        json.dump(data, f)
+
+def list_images():
+    data = load_image_data()
+    for id, info in data.items():
+        console.print(f"Nombre: {info['filename']},ID: {id}, Tamaño: {info['size']}")
+
+
+
+def upload_image():
+    # Cargar los datos de las imágenes ya subidas
+    uploaded_images = load_image_data()
+
+    # Listar los archivos en el directorio de imágenes
+    files = os.listdir(IMAGE_DIR)
+
+    # Filtrar la lista para excluir las imágenes ya subidas
+    available_files = [f for f in files if f not in uploaded_images.values()]
+
+    # Mostrar las imágenes disponibles
+    console.print("Imágenes disponibles para subir:")
+    for i, filename in enumerate(available_files, start=1):
+        console.print(f"{i}. {filename}")
+
+    # Pedir al usuario que seleccione una imagen para subir
+    file_index = int(console.input("Seleccione el número de la imagen a subir: ")) - 1
+    filename = available_files[file_index]
+
+    # Generar un ID único para la imagen
+    id = str(uuid.uuid4())
+
+    # Obtener el tamaño del archivo
+    filepath = os.path.join(IMAGE_DIR, filename)
+    size = os.path.getsize(filepath)
+
+    # Guardar los datos de la imagen
+    uploaded_images[id] = {'filename': filename, 'size': size}
+    save_image_data(uploaded_images)
+
+    console.print(f"[bold green]Imagen {filename} subida con éxito con el ID {id}.[/]")
+
+
+
+
+
 
 console = Console()
 
@@ -19,125 +83,86 @@ def prompt_for_choice(options):
     return choice
 
 def login():
-    while True:
-        console.input("Usuario: ")
-        console.input("Contraseña: ", password=True)
-        console.print("[bold green]Inicio de sesión exitoso.[/]")
-        break
+    # Establecer la conexión con la base de datos
+    cnx = mariadb.connect(user='root', password='Cisco12345',
+                                  host='127.0.0.1',
+                                  database='mydb')
+    cursor = cnx.cursor()
 
-def main_menu():
+    while True:
+        username = console.input("Usuario: ")
+        password = console.input("Contraseña: ", password=True)
+
+        # Generar el hash SHA-256 de la contraseña
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+        # Ejecutar la consulta SQL
+        query = ("SELECT username, password, rol FROM usuario WHERE username = %s AND password = %s")
+        cursor.execute(query, (username, password_hash))
+
+        # Verificar si el usuario existe
+        user = cursor.fetchone()
+        if user is not None:
+            console.print("[bold green]Inicio de sesión exitoso.[/]")
+            return user[2]
+        else:
+            console.print("[bold red]Usuario o contraseña incorrectos, por favor intente de nuevo.[/]")
+
+    # Cerrar la conexión con la base de datos
+    cursor.close()
+    cnx.close()
+
+def main_menu(role):
     options = {
-        '1': "Gestión de VMs",
-        '2': "Monitorización de Recursos",
-        '3': "Gestión de Seguridad",
-        '4': "Configuración de Networking",
-        '5': "Operaciones de Almacenamiento",
-        '6': "Gestión de Usuarios",
-        '7': "Registro y Auditoría",
-        '8': "Salir"
+        '1': "Gestión de Slices",
+        '2': "Salir"
     }
+    if role == 'admin':
+        options['3'] = "Gestión de Usuarios"
+        options['4'] = "Administrar imágenes"
     display_menu("Menú Principal", options)
     return prompt_for_choice(options)
 
 def handle_choice(choice):
     menu_functions = {
-        '1': vm_management,
-        '2': resource_monitoring,
-        '3': security_management,
-        '4': networking_configuration,
-        '5': storage_operations,
-        '6': user_management,
-        '7': audit_logs,
-        '8': lambda: None  # Para salir
+        '1': slice_management,
+        '2': lambda: None,  # Para salir
+        '3': user_management if choice == '3' else lambda: console.print("[red]Opción no válida.[/]"),
+        '4': image_management if choice == '4' else lambda: console.print("[red]Opción no válida.[/]")
     }
     func = menu_functions.get(choice, lambda: console.print("[red]Opción no válida.[/]"))
     return func()
-#
-def manage_submenu(options):
+
+def image_management():
     while True:
-        display_menu("Submenú", options)
+        options = {
+            '1': "Listar imágenes",
+            '2': "Subir imagen",
+            '3': "Regresar al Menú Principal"
+        }
+        display_menu("Administrar imágenes", options)
         choice = prompt_for_choice(options)
-        if choice.endswith('6') or choice.endswith('5'):
+        if choice == '1':
+            list_images()
+        elif choice == '2':
+            upload_image()
+        elif choice == '3':
             break
-        console.print(f"[bold green]Acción seleccionada: {options[choice]}[/]")
 
-def vm_management():
-    options = {
-        '1.1': "Crear VM",
-        '1.2': "Listar todas las VMs",
-        '1.3': "Ver detalles de una VM",
-        '1.4': "Modificar una VM",
-        '1.5': "Eliminar una VM",
-        '1.6': "Regresar al Menú Principal"
-    }
-    manage_submenu(options)
 
-def resource_monitoring():
-    options = {
-        '2.1': "Ver uso actual de CPU",
-        '2.2': "Ver uso actual de Memoria",
-        '2.3': "Ver almacenamiento disponible",
-        '2.4': "Ver estado de red",
-        '2.5': "Regresar al Menú Principal"
-    }
-    manage_submenu(options)
-
-def security_management():
-    options = {
-        '3.1': "Administrar reglas de firewall",
-        '3.2': "Configurar VPN",
-        '3.3': "Revisar políticas de seguridad",
-        '3.4': "Auditar accesos y eventos de seguridad",
-        '3.5': "Regresar al Menú Principal"
-    }
-    manage_submenu(options)
-
-def networking_configuration():
-    options = {
-        '4.1': "Crear red virtual",
-        '4.2': "Modificar configuración de red",
-        '4.3': "Eliminar red virtual",
-        '4.4': "Ver topología de red",
-        '4.5': "Regresar al Menú Principal"
-    }
-    manage_submenu(options)
-
-def storage_operations():
-    options = {
-        '5.1': "Aprovisionar almacenamiento",
-        '5.2': "Expandir almacenamiento existente",
-        '5.3': "Liberar espacio de almacenamiento",
-        '5.4': "Ver informes de uso de almacenamiento",
-        '5.5': "Regresar al Menú Principal"
-    }
-    manage_submenu(options)
+def slice_management():
+    console.print("[bold green]Acción seleccionada: Gestión de Slices[/]")
 
 def user_management():
-    options = {
-        '6.1': "Crear nuevo usuario",
-        '6.2': "Listar usuarios",
-        '6.3': "Modificar usuario",
-        '6.4': "Eliminar usuario",
-        '6.5': "Regresar al Menú Principal"
-    }
-    manage_submenu(options)
-
-def audit_logs():
-    options = {
-        '7.1': "Ver logs del sistema",
-        '7.2': "Exportar logs para auditoría",
-        '7.3': "Configurar niveles de log",
-        '7.4': "Regresar al Menú Principal"
-    }
-    manage_submenu(options)
+    console.print("[bold green]Acción seleccionada: Gestión de Usuarios[/]")
 
 @click.command()
 def main():
     """Inicia la aplicación automáticamente al ejecutar el script."""
-    login()
+    role = login()
     while True:
-        choice = main_menu()
-        if choice == '8':
+        choice = main_menu(role)
+        if choice == '2':
             console.print("[bold green]Saliendo del sistema...[/]")
             break
         handle_choice(choice)
