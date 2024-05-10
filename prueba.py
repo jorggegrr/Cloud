d5fc516250f9c73816e40ced1fe280836c3173df
Angel <angel.bautista@pucp.edu.pe>
2024-05-10 18:19:56 +0000

    grafica de topologia

--- prueba.py
+++ prueba.py
@@ -1,8 +1,11 @@
+from PIL import Image
 import click
 import networkx as nx
 import matplotlib.pyplot as plt
 from rich.console import Console
 from rich.table import Table
+import tempfile
+import os
 
 console = Console()
 
@@ -19,6 +22,11 @@ def prompt_for_choice(options):
         if choice not in options:
             console.print("[bold red]Opción inválida, por favor intente de nuevo.[/]")
     return choice
+
+
+
+
+
 
 def tree_topology():
     console.print("[bold green]Creando una topología tipo árbol[/]")
@@ -38,9 +46,29 @@ def tree_topology():
     # Dibujar el árbol
     pos = nx.spring_layout(G)  # Posicionamiento de los nodos
     nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=500, font_size=8)
-    plt.show()
+
+    # Guardar la imagen en un archivo temporal
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp:
+        plt.savefig(temp.name)
+        temp_image_name = temp.name
+
+    # Redimensionar la imagen
+    img = Image.open(temp_image_name)
+    img = img.resize((img.width * 2, img.height * 2), Image.LANCZOS)  # Cambia ANTIALIAS por LANCZOS
+    img.save(temp_image_name)
+
+    # Mostrar la imagen con viu
+    os.system(f'viu {temp_image_name}')
+
+    # Eliminar el archivo temporal
+    os.remove(temp_image_name)
 
     console.print(f"[bold green]Topología tipo árbol creada con {num_branches} ramas y {num_levels} niveles.[/]")
+
+
+
+
+
 
 def topology_management():
     while True:
